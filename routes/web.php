8a52f7ed45c605ce34e01e2a45777322a754e660
agthm3 <gigam57@gmail.com>
2024-06-26 22:12:35 +0800

    change route default

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.home.detail-show');
+    return view('pages.home.index');
 });
 
 Route::get('/home', [HomeController::class, 'index'])->name('home.index');
