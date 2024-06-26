942b73387ede995e3e3f3a8dfa4868af73b3c5aa
agthm3 <gigam57@gmail.com>
2024-06-26 14:53:38 +0800

    add font google

---
+++ resources/views/pages/home/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="destination-card">
+        <h5>Mau Kemana Hari ini?</h5>
+        <p>Pilih destinasi kamu dibawah ini.</p>
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1"><i class="fas fa-map-marker-alt"></i></span>
+            </div>
+            <input type="text" class="form-control" placeholder="Pintu Nol, Universitas Hasanuddin" aria-label="Destination" aria-describedby="basic-addon1">
+        </div>
+    </div>
+
+    <div class="oto-card">
+        <div class="oto-card-content">
+            <h5>OTO - F1</h5>
+            <p>Malengkeri Kampus Unhas</p>
+            <button class="btn btn-light">Pilih</button>
+        </div>
+        <img src="asset/img/otogo-mobil.svg" alt="Kendaraan">
+    </div>
+
+    <div class="oto-card">
+        <div class="oto-card-content">
+            <h5>OTO - E1</h5>
+            <p>Terminal Panakkukang Kampus Unhas</p>
+            <button class="btn btn-light">Pilih</button>
+        </div>
+        <img src="asset/img/otogo-mobil.svg" alt="Kendaraan">
+    </div>
+</div>
+@endsection
---
+++ resources/views/pages/home/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="{{ asset('asset/css/style.css') }}" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="login-body">
+
+    <div class="login-container">
+        <img src="asset/img/logo-login.svg" alt="Logo" class="logo">
+        <img src="asset/img/otogo-login.svg" alt="Vehicle" class="vehicle">
+        <button class="btn">Masuk dengan akun yang sudah ada</button>
+        <p>Belum punya akun? <a href="#">Registrasi disini</a></p>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
