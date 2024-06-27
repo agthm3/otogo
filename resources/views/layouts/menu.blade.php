2fb4cf67c7fe02187520ae9a081eb618454cee31
agthm3 <gigam57@gmail.com>
2024-06-27 20:55:16 +0800

    add menu name under icon

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,6 +1,18 @@
 <div class="nav-bar">
-    <a href="{{ route('home.index') }}"><img src="{{ asset('asset/img/icon/home-on.png') }}" alt="Home"></a>
-    <a href="{{ route('direction.index') }}"><img src="{{ asset('asset/img/icon/direction-off.png') }}" alt="Direction"></a>
-    <a href="{{ route('ticket.index') }}"><img src="{{ asset('asset/img/icon/ticket-off.png') }}" alt="ticket"></a>
-    <a href="{{ route('setting.index') }}"><img src="{{ asset('asset/img/icon/settings-off.png') }}" alt="Setting"></a>
+    <a href="{{ route('home.index') }}">
+        <img src="{{ asset('asset/img/icon/home-on.png') }}" alt="Home">
+        <span>Home</span>
+    </a>
+    <a href="{{ route('direction.index') }}">
+        <img src="{{ asset('asset/img/icon/direction-off.png') }}" alt="Direction">
+        <span>Direction</span>
+    </a>
+    <a href="{{ route('ticket.index') }}">
+        <img src="{{ asset('asset/img/icon/ticket-off.png') }}" alt="Ticket">
+        <span>Ticket</span>
+    </a>
+    <a href="{{ route('setting.index') }}">
+        <img src="{{ asset('asset/img/icon/settings-off.png') }}" alt="Setting">
+        <span>Setting</span>
+    </a>
 </div>
